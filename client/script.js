0ee8ab621a0476f7f34569d380338190c79f6522
bojin11 <15011278723@163.com>
2022-01-26 18:11:31 +0000

    changes

---
+++ client/script.js
@@ -0,0 +1,57 @@
+
+const searchButton = document.querySelector('.submit')
+const form = document.querySelector('#search-form')
+
+  const  init= async (e) => {
+   
+        e.preventDefault();
+        
+        const randonInput = e.target.text.value;
+
+        if(randonInput.toLowerCase() === "random")
+        {
+            getRandom();
+        }
+        try{
+
+      
+
+            const response = await fetch('http://localhost:3000/artists')
+    
+            const data = await response.json()
+    
+            setData(data)
+            
+            
+    
+        } catch (err) {
+            console.log(err)
+        }
+    }
+  
+    function getRandom(){
+
+      fetch('http://localhost:3000/artists/random')
+        .then(response => response.json()) 
+        .then(json => setData(json))
+
+    }
+    function setData(artists){
+        artists.forEach((artist) => appendSearch(artist));
+    };
+
+  function appendSearch(data){
+    const newLi = document.createElement('li');
+    const artistLink= document.createElement('a')
+    artistLink.setAttribute('class', 'artist-link')
+    artistLink.setAttribute('href', `${data.url}`)
+    const artistsList = document.querySelector('#search-result');
+    artistLink.textContent = `Artist: ${data.name}`;
+    newLi.appendChild(artistLink);
+    artistsList.append(newLi);
+  }
+  
+  
+  
+form.addEventListener('submit', init)
+  
