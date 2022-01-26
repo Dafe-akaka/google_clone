2412c5214d499ff8f2e110c47f1befa962e557e5
Dafe-akaka <dafe.akaka123@gmail.com>
2022-01-26 13:37:00 +0000

    setup database

--- server/data.js
+++ server/data.js
@@ -1,1 +1,24 @@
-artists= []
+ const artists = [
+
+    {id: 1, name:'The Weeknd' , sex: 'male', spotifyUrl: 'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ'},  
+    {id: 2, name: 'Justin Bieber', sex: 'male', spotifyUrl  : 'https://open.spotify.com/artist/1uNFoZAHBGtllmzznpCI3s'},
+    {id: 3, name: 'Ed Sheeran', sex: 'male', spotifyUrl:'https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V'},
+    {id: 4, name: '	Dua Lipa', sex: 'female', spotifyUrl: 'https://open.spotify.com/artist/6M2wZ9GZgrQXHCFfjv46we'},
+    {id: 5, name: 'Adele', sex: 'female', spotifyUrl:'https://open.spotify.com/artist/4dpARuHxo51G3z768sgnrY'},
+    {id: 6, name: 'Coldplay', sex: 'male', spotifyUrl:'https://open.spotify.com/artist/4gzpq5DPGxSnKTe4SA8HAU'},
+    {id: 7, name: '	Ariana Grande', sex: 'female', spotifyUrl:'https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR'},
+    {id: 8, name: 'Doja Cat', sex: 'female', spotifyUrl:'https://open.spotify.com/artist/5cj0lLjcoR7YOSnhnX0Po5'},
+    {id: 9, name: 'Imagine Dragons', sex: 'male', spotifyUrl: 'https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q'},
+    {id: 10, name:'Bruno Mars' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/0du5cEVh5yTK9QJze8zA0C'},
+    {id: 11, name:'	Taylor Swift' , sex: 'female', spotifyUrl:'https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02'},
+    {id: 12, name:'Elton John' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/3PhoLpVuITZKcymswpck5b'},
+    {id: 13, name:'	Drake' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4'},    
+    {id: 14, name:'Lil Nas X' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/7jVv8c5Fj3E9VhNjxT4snq'},
+    {id: 15, name:'J Balvin' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/1vyhD5VmyZ7KMfW5gqLgo5'},
+    {id: 16, name:'The Kid Laroi' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/2tIP7SsRs7vjIcLrU85W8J'},
+    {id: 17, name:'David Guetta' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/1Cs0zKBU1kc0i8ypK3B9ai'},
+    {id: 18, name:'Billie Eilish' , sex: 'female', spotifyUrl:'https://open.spotify.com/artist/6qqNVTkY8uBg9cP3Jd7DAH'},
+    {id: 19, name:'Post Malone' , sex: 'male', spotifyUrl: 'https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60'},
+    {id: 20, name:'Maroon 5' , sex: 'male', spotifyUrl:'https://open.spotify.com/artist/04gDigrS5kc9YWfZHwBETP'},
+]   
+   
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const {app} = require('./server');
+const port = 3000;
+
+// To do: Get the server running
+app.listen(port, () => { console.log(`Express now listening on ${port}`)})
